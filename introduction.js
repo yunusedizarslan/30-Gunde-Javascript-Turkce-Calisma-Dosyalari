c4c3b827c06dcdaf21f303f4ebc987a30da66d80
Yunus Edizarslan <110184643+yunusedizarslan@users.noreply.github.com>
2022-08-18 16:30:56 +0300

    Javascript Cagirma Metodlari ve ilk js kodu

---
+++ introduction.js
@@ -0,0 +1,9 @@
+let name = "Yunus"
+let surName = "Edizarslan"
+let age = 24
+let married = true 
+
+console.log("name :", name , "Surname : ", surName , "Age : ", age , "İs he or she married ? ", married)
+
+const pi = 3.14
+console.log(pi)
